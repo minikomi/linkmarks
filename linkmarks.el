c99f84cdbb9ea526f9627c2212caf8cfb5323876
Adam Moore <adam-m@mercari.com>
2024-09-03 11:27:25 +0900

    fixes

--- linkmarks.el
+++ linkmarks.el
@@ -3,8 +3,8 @@
 
 ;; Author: Dustin Lacewell <dlacewell@gmail.com>
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24") (helm "0") (dash "0") (org "0"))
-;; Keywords: bookmarks, org, helm
+;; Package-Requires: ((emacs "24") (org "0"))
+;; Keywords: bookmarks, org
 ;; URL: http://github.com/dustinlacewell/linkmarks
 
 ;;; Commentary:
@@ -13,71 +13,38 @@
 
 ;;; Code:
 (require 'cl-lib)
-(require 'dash)
 (require 'org)
-(require 'seq)
 
 ;;;###autoload
 (defgroup linkmarks nil
   "Configuration options for linkmarks."
   :group 'org)
 
-;;;###autoload
-(defcustom linkmarks-file (expand-file-name "~/org/bookmarks.org")
-  "Define linkmarks file to store your bookmarks in."
-  :group 'linkmarks
-  :type 'string)
-
-(cl-defun linkmarks--setup ()
-  (setq org-outline-path-complete-in-steps nil)
-  (setq org-link-frame-setup '((elisp . find-file)
-                               (vm . vm-visit-folder-other-frame)
-                               (vm-imap . vm-visit-imap-folder-other-frame)
-                               (gnus . org-gnus-no-new-news)
-                               (file . find-file)
-                               (wl . wl-other-frame)))
-  (setq org-refile-use-outline-path t)
-  (setq org-refile-targets `((,linkmarks-file :maxlevel . 99)))
-  (setq org-confirm-elisp-link-function nil)
-  (setq safe-local-variable-values '((org-confirm-elisp-link-function . nil))))
-
 (cl-defun linkmarks--in-file ()
-  (linkmarks--setup)
-  (with-temp-buffer
-    (insert-file-contents linkmarks-file t)
-    (org-mode)
-    (goto-char (point-min))
-    (outline-show-all)
-    (cl-loop
-     for target in (org-refile-get-targets)
-     for element = (progn
-                     (goto-char (nth 3 target))
-                     (forward-line)
-                     (org-element-context))
-     for type = (car element)
-     for props = (cadr element)
-     for begin = (plist-get props :begin)
-     for end = (plist-get props :end)
-     for content = (buffer-substring begin end)
-     if (equal 'link type)
-     collect (list (car target) content))))
+  (cl-loop
+   for target in (org-refile-get-targets)
+   for element = (with-current-buffer (find-buffer-visiting (nth 1 target))
+                   (goto-char (nth 3 target))
+                   (org-end-of-meta-data t)
+                   (org-element-context))
+   for type = (car element)
+   for props = (cadr element)
+   for link = (plist-get props :raw-link)
+   if (and (equal 'link type) link)
+   collect (list (car target) link target)))
 
 ;;;###autoload
-(cl-defun linkmarks-select ()
-  (interactive)
-  (-let* ((targets (linkmarks--in-file))
-          (choices (mapcar 'car targets))
-          (choice (completing-read "Bookmark: " choices))
-          ((_ link) (-first (lambda (i) (equal (car i) choice)) targets)))
-    (org-open-link-from-string link)))
+(cl-defun linkmarks-select (arg)
+  (interactive "P")
+  (let* ((targets (linkmarks--in-file))
+         (choice (assoc (completing-read "Bookmark: " (mapcar 'car targets)) targets)))
+    (if (not arg) (org-link-open-from-string (cadr choice))
+      (let ((buffer (find-buffer-visiting (nth 1 (nth 2 choice))))
+            (point (nth 3 (nth 2 choice))))
+        (message "Jumping to Heading \"%s\" in buffer %s at %s" (car choice) buffer point)
+        (pop-to-buffer buffer)
+        (goto-char point)))))
 
-;;;###autoload
-(defun linkmarks-capture ()
-  (interactive)
-  (let ((org-capture-templates '(("t" "Bookmark" entry (file linkmarks-file)
-                             "* %^{Title}\n[[%?]]\n  added: %U" :kill-buffer t))))
-    (linkmarks--setup)
-    (org-capture)))
 
 (provide 'linkmarks)
 
